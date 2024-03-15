0c5e36307e5861186df9039442fc9100d8ba74c0
Luke Youell <74372554+luke-parallax@users.noreply.github.com>
2024-03-15 15:11:01 +0000

    Import config facade

--- src/Models/FilamentComment.php
+++ src/Models/FilamentComment.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Config;
 
 class FilamentComment extends Model
 {
