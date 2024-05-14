360550f8165b4ae74420202a87ccd3b028b7a42d
Amirali Kalantari Pour <amiralidev@gmail.com>
2024-05-14 13:56:33 +0330

    Add Persian translation
    Added missing Persian translations.

---
+++ resources/lang/fa/filament-comments.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'comments' => 'نظرات',
+    'comments.add' => 'اضافه کردن نظر',
+    'comments.empty' => 'هنوز نظری وجود ندارد.',
+    'comments.placeholder' => 'یک نظر اضافه کنید...',
+
+    'notifications.created' => 'نظر اضافه شد.',
+    'notifications.deleted' => 'نظر حذف شد.',
+
+    'modal.heading' => 'نظرات',
+];
