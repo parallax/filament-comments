10e64ce91570499da4efdad939356516059fe73c
agencetwogether <53862310+agencetwogether@users.noreply.github.com>
2024-09-08 21:34:03 +0200

    French translation

---
+++ resources/lang/fr/filament-comments.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'comments' => 'Commentaires',
+    'comments.add' => 'Ajouter un commentaire',
+    'comments.empty' => 'Pas encore de commentaires.',
+    'comments.placeholder' => 'Ajouter un commentaire...',
+
+    'notifications.created' => 'Commentaire ajouté.',
+    'notifications.deleted' => 'Commentaire supprimé.',
+
+    'modal.heading' => 'Commentaires',
+];
